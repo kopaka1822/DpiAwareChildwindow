de8baadabfe788ffbad8f57b131fbccd407f7dab
Felix Brüll <felixbruell@live.de>
2020-01-05 17:17:03 +0100

    initial commit

---
+++ DpiAwareSubwindow/ChildWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+
+namespace DpiAwareSubwindow
+{
+    public class ChildWindow : HwndHost
+    {
+        private readonly Border parent;
+        private IntPtr hWnd = IntPtr.Zero;
+
+        public SwapChain SwapChain { get; private set; }
+
+        public ChildWindow(Border parent)
+        {
+            this.parent = parent;
+            parent.SizeChanged += ParentOnSizeChanged;
+        }
+
+        private void ParentOnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // problem: width and height is not correctly scaled
+            SwapChain?.Resize((int)(parent.ActualWidth), (int)(parent.ActualHeight));
+        }
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            // create subwindow
+            hWnd = CreateWindowEx(
+                0, // dwstyle
+                "static", // class name
+                "", // window name
+                WS_CHILD | WS_VISIBLE, // style
+                0, // x
+                0, // y
+                (int)parent.ActualWidth, // renderWidth
+                (int)parent.ActualHeight, // renderHeight
+                hwndParent.Handle, // parent handle
+                IntPtr.Zero, // menu
+                IntPtr.Zero, // hInstance
+                0 // param
+            );
+
+            // directx swap chain
+            // problem: width and height is not correctly scaled
+            SwapChain = new SwapChain(hWnd, (int)parent.ActualWidth, (int)parent.ActualHeight);
+
+            return new HandleRef(this, hWnd);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            DestroyWindow(hWnd);
+        }
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        internal static extern bool DestroyWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateWindowEx(
+            int dwExStyle,
+            string lpszClassName,
+            string lpszWindowName,
+            int style,
+            int x, int y,
+            int width, int height,
+            IntPtr hwndParent,
+            IntPtr hMenu,
+            IntPtr hInst,
+            [MarshalAs(UnmanagedType.AsAny)] object pvParam
+        );
+
+        internal const int
+            WS_CHILD = 0x40000000,
+            WS_VISIBLE = 0x10000000;
+    }
+}
---
+++ DpiAwareSubwindow/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharpDX.Mathematics.Interop;
+
+namespace DpiAwareSubwindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ChildWindow child;
+        private float childColor = 1.0f;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            child = new ChildWindow(BorderHost);
+            BorderHost.Child = child;
+        }
+
+        private void MainWindow_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (child == null) return;
+            if (child.SwapChain == null) return;
+
+            // switch between black and white background
+            childColor = 1.0f - childColor;
+
+            // clear child background
+            child.SwapChain.BeginFrame();
+            Device.Get().ClearRenderTargetView(child.SwapChain.Rtv, new RawColor4(childColor, childColor, childColor, childColor));
+            child.SwapChain.EndFrame();
+        }
+    }
+}
